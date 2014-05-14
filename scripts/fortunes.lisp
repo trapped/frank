c39547a1e1f5394fc3b45ffd387e48919a6eedb9
trapped <giorgio.pellero@gmail.com>
2014-05-14 23:37:24 +0200

    Fortune: -iso instead of -is

--- scripts/fortunes.lisp
+++ scripts/fortunes.lisp
@@ -4,13 +4,13 @@
 ;;; Outputs short-medium
 ;;; fortunes from fortune(1)
 ;;; (fortune-mod).
-;;; -is
+;;; -iso
 ;;;
 
 (defun do-fortune(chan sender arg)
   "Tells fortunes. Usage: fortune"
   (with-open-stream
-	(s1 (ext:run-program "fortune" :arguments '("-is") :output :stream))
+	(s1 (ext:run-program "fortune" :arguments '("-iso") :output :stream))
 	(with-output-to-string (out)
 	  (progn
 		(copy-stream s1 out)
